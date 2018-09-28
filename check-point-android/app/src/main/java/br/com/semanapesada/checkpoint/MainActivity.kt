36c4fe57856b5ee2c4749c71b74d6ba5c972bc7c
Alfredo Porfírio <cin_aporfirio@uolinc.com>
2018-09-28 19:24:20 -0300

    Adicionando place picker

--- check-point-android/app/src/main/java/br/com/semanapesada/checkpoint/MainActivity.kt
+++ check-point-android/app/src/main/java/br/com/semanapesada/checkpoint/MainActivity.kt
@@ -1,17 +1,16 @@
 package br.com.semanapesada.checkpoint
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import br.com.semanapesada.checkpoint.fragment.CheckFragment
 import br.com.semanapesada.checkpoint.fragment.ConfigFragment
-import br.com.semanapesada.checkpoint.fragment.LocationFragment
+import br.com.semanapesada.checkpoint.fragment.PlacePickerFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private val itemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
                 // message.setText(R.string.title_home)
@@ -25,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.navigation_notifications -> {
                 // message.setText(R.string.title_notifications)
-                supportFragmentManager.beginTransaction().replace(R.id.containerFragment, LocationFragment()).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.containerFragment, PlacePickerFragment()).commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -36,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(itemSelectedListener)
 
         supportFragmentManager.beginTransaction().add(R.id.containerFragment, ConfigFragment()).commit()
     }
